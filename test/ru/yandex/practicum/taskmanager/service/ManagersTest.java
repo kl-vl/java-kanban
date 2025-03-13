1edf2e110d62d9c4359fad8894c7a76c01af629d
kl-vl <191594458+kl-vl@users.noreply.github.com>
2025-03-13 18:30:37 +0300

    refact: ManagerTest methods name corrections

--- test/ru/yandex/practicum/taskmanager/service/ManagersTest.java
+++ test/ru/yandex/practicum/taskmanager/service/ManagersTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class ManagersTest {
 
     @Test
-    void getDefault_shouldReturnNonNullTaskManagerWithBasicFunctionality() {
+    void getDefault_shouldReturnNonNullTaskManager() {
         TaskManager taskManager = Managers.getDefault();
 
         assertNotNull(taskManager, "TaskManager should not be null");
     }
 
     @Test
-    void getDefaultHistory_shouldReturnNonNullHistoryManagerWithBasicFunctionality() {
+    void getDefaultHistory_shouldReturnNonNullHistoryManager() {
         HistoryManager historyManager = Managers.getDefaultHistory();
 
         assertNotNull(historyManager, "HistoryManager should not be null");
